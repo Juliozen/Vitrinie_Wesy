c5fd9af58e6ed3b412e583e623bead9059b4bc06
Vinicius de Borba <vinicius_deborba@hotmail.com>
2018-02-07 14:52:20 -0200

    ajax novo

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
 $(document).ready(function () {
-    $.ajax({
-        type: "GET",
-        url: "http://backend.api.com/vitrine/get",
-        data: {'Content-Type' : 'application/json', 'Authorization-Token' : 'testtoken' },
-        success: function (data) {
-            data = JSON.parse(data);
 
-            $('#vitrine').append('<div class="swiper-container">\n' +
-                '    <div class="swiper-wrapper">\n' +
-                '      <div class="swiper-slide">Slide 1</div>\n' +
-                '      <div class="swiper-slide">Slide 2</div>\n' +
-                '      <div class="swiper-slide">Slide 3</div>\n' +
-                '      <div class="swiper-slide">Slide 4</div>\n' +
-                '      <div class="swiper-slide">Slide 5</div>\n' +
-                '      <div class="swiper-slide">Slide 6</div>\n' +
-                '      <div class="swiper-slide">Slide 7</div>\n' +
-                '      <div class="swiper-slide">Slide 8</div>\n' +
-                '      <div class="swiper-slide">Slide 9</div>\n' +
-                '      <div class="swiper-slide">Slide 10</div>\n' +
-                '    </div>\n' +
-                '    <!-- Add Pagination -->\n' +
-                '    <div class="swiper-pagination"></div>\n' +
-                '  </div>');
-        },
-        error: function () {
+    jQuery(function ($) {
+        $.ajax({
+            type: "GET",
+            url: "http://backend.api.com/vitrine/get",
+            data: {'Content-Type' : 'application/json', 'Authorization-Token' : 'testtoken' },
+            success: function (data) {
+                data = JSON.parse(data);
 
-        }
+                $('#vitrine').append('<div class="swiper-container">\n' +
+                    '    <div class="swiper-wrapper">\n' +
+                    '      <div class="swiper-slide">Slide 1</div>\n' +
+                    '      <div class="swiper-slide">Slide 2</div>\n' +
+                    '      <div class="swiper-slide">Slide 3</div>\n' +
+                    '      <div class="swiper-slide">Slide 4</div>\n' +
+                    '      <div class="swiper-slide">Slide 5</div>\n' +
+                    '      <div class="swiper-slide">Slide 6</div>\n' +
+                    '      <div class="swiper-slide">Slide 7</div>\n' +
+                    '      <div class="swiper-slide">Slide 8</div>\n' +
+                    '      <div class="swiper-slide">Slide 9</div>\n' +
+                    '      <div class="swiper-slide">Slide 10</div>\n' +
+                    '    </div>\n' +
+                    '    <!-- Add Pagination -->\n' +
+                    '    <div class="swiper-pagination"></div>\n' +
+                    '  </div>');
+            },
+            error: function () {
+
+            }
+        });
     });
+
 }
