7743442dd437c982816d952715e2fa9dbde6eb3a
Vinicius de Borba <vinicius_deborba@hotmail.com>
2018-02-08 16:11:54 -0200

    [Develop] - Showcase with API

--- main.js
+++ main.js
@@ -1,52 +1,83 @@
-
 $(document).ready(function () {
 
     jQuery(function ($) {
-      $.getScript("https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.1.0/js/swiper.min.js",
-      function() {
-         // alert("Script loaded but not necessarily executed.");
+        $.getScript("https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.1.0/js/swiper.min.js",
+            function () {
+                // alert("Script loaded but not necessarily executed.");
 
-        $.ajax({
-            type: "GET",
-            url: "http://backend.api.com/vitrine/get",
-            headers: {
-                'Content-Type' : 'application/json', 'Authorization-Token' : 'testtoken'
-            },
+                $.ajax({
+                    type: "GET",
+                    url: "http://backend.api.com/vitrine/get",
+                    headers: {
+                        'Content-Type': 'application/json', 'Authorization-Token': 'testtoken'
+                    },
 
-            success: function (data) {
+                    success: function (data) {
+
+                        console.log(data.resposta);
+
+                        var vitrine = data.resposta;
 
 
-                $('#vitrine').append('<div class="swiper-container">\n' +
-                    '    <div class="swiper-wrapper">\n' +
-                    '      <div class="swiper-slide">Slide 1</div>\n' +
-                    '      <div class="swiper-slide">Slide 2</div>\n' +
-                    '      <div class="swiper-slide">Slide 3</div>\n' +
-                    '      <div class="swiper-slide">Slide 4</div>\n' +
-                    '      <div class="swiper-slide">Slide 5</div>\n' +
-                    '      <div class="swiper-slide">Slide 6</div>\n' +
-                    '      <div class="swiper-slide">Slide 7</div>\n' +
-                    '      <div class="swiper-slide">Slide 8</div>\n' +
-                    '      <div class="swiper-slide">Slide 9</div>\n' +
-                    '      <div class="swiper-slide">Slide 10</div>\n' +
-                    '    </div>\n' +
-                    '    <!-- Add Pagination -->\n' +
-                    '    <div class="swiper-pagination"></div>\n' +
-                    '  </div>');
-                    var swiper = new Swiper('.swiper-container', {
-                      slidesPerView: 4,
-                      spaceBetween: 30,
-                      centeredSlides: true,
-                      pagination: {
-                        el: '.swiper-pagination',
-                        clickable: true,
-                      },
-                    });
-            },
-            error: function () {
+                        $('#vitrine').append('<div class="swiper-container">' +
+                            '    <div id="teste" class="swiper-wrapper"></div>' +
 
-            }
-        });
+                            '    <div class="swiper-button-next"></div>\n' +
+                            '    <div class="swiper-button-prev"></div>\n' +
+                            '    <div class="swiper-pagination"></div>\n' +
+                            '    <div class="swiper-scrollbar"></div>\n' +
+                            '  </div>');
 
+                        $("#teste").css({"font-size": vitrine.labels[0].fontSize,
+                            'color': vitrine.labels[0].fontColor,
+                            "font-family" : vitrine.fontFamily });
+
+                        var pagination = vitrine.passIndicator[0].not === 0 ?
+                            vitrine.passIndicator[0].line === 0 ? '.swiper-pagination' : null : null;
+
+                        var scrollbar = vitrine.passIndicator[0].not === 0 ?
+                            vitrine.passIndicator[0].line === 1 ? '.swiper-scrollbar' : null : null;
+
+                        var autoplay = vitrine.autoLoop === 1 ? {
+                            delay: 1000,
+                            disableOnInteraction: false
+                        } : false;
+
+                        var swiper = new Swiper('.swiper-container', {
+                            slidesPerView: vitrine.grid.narrow,
+                            spaceBetween: 0,
+                            centeredSlides: false,
+                            mousewheel: vitrine.mouseWeel === 1,
+                            direction: vitrine.indicator[0].vertical ? 'vertical' : 'horizontal',
+                            pagination: {
+                                el: pagination,
+                                clickable: true
+                            },
+                            autoplay: autoplay,
+                            scrollbar: {
+                                el: scrollbar,
+                                hide: false
+                            },
+                            navigation: {
+                                nextEl: vitrine.arrows === 1 ? '.swiper-button-next' : null,
+                                prevEl: vitrine.arrows === 1 ? '.swiper-button-prev' : null
+                            },
+                            virtual: {
+                                slides: (function () {
+                                    var slides = [];
+                                    for (var i = 0; i < 3; i++) {
+                                        slides.push('Slide ' + (i + 1));
+                                    }
+                                    return slides;
+                                }())
+                            }
+                        });
+                    },
+                    error: function () {
+
+                    }
+                });
+
+            });
     });
-  });
 });
